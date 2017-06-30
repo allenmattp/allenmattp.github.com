c7261b20f50f56847c5e4afca2d9bb0c56389510
allenmattp <allen.mattp@gmail.com>
2017-06-29 21:30:09 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,9 +5,30 @@ function main() {
 };
 
 $('div.menu').hover(function() {
-		$(this).addClass("hover");
+		$(this).addClass('hover');
 	}, function() {
-		$(this).removeClass("hover")
+		$(this).removeClass('hover')
 	});
+
+$('#about').click(function() {
+	$ (this).addClass('hover');
+	$ ('#projects').removeClass('hover')
+	$ ('#contact').removeClass('hover')
+	$ ('.content').replaceWith('<div class="content"><p>This is text talking about me</p></div>');
+});
+
+$('#projects').click(function() {
+	$ (this).addClass('hover');
+	$ ('#about').removeClass('hover')
+	$ ('#contact').removeClass('hover')
+	$ ('.content').replaceWith('<div class="content"><p>These are some of my projects.</p></div>');
+});
+
+$('#contact').click(function() {
+	$ (this).addClass('hover');
+	$ ('#about').removeClass('hover')
+	$ ('#projects').removeClass('hover')
+	$ ('.content').replaceWith('<div class="content"><p>These are some ways to contact me!</p></div>');
+});
 
 $(document).ready(main);
